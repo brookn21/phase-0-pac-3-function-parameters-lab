47d263851668d62bdc07cd8733b34989165be15e
Brook Nega <brooknega21@gmail.com>
2022-08-31 14:11:27 -0400

    commit tree f7415bc6f688f0e679a0b9afb88ca6e7cd02286e

---
+++ index.js
@@ -0,0 +1,8 @@
+function introduction(name){
+    if(name === "Aki"){
+        return("Hi, my name is Aki.")
+    }
+    else if( name === "Samip"){
+        return("Hi, my name is Samip.")
+    }
+}
